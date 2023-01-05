40a1be19ee6c5a2a2b8a3216952a6bfd78c3b6a3
Justin Caryk <cary9983@gmail.com>
2023-01-05 00:33:03 -0800

    better opacity

--- app/(private)/companions/layout.tsx
+++ app/(private)/companions/layout.tsx
@@ -7,7 +7,7 @@ export default function RangerLayout({
 }) {
   return (
     <div>
-      <div className='bg-hero-3 h-full w-full bg-no-repeat bg-contain bg-top fixed right-0 top-24 -z-10' />
+      <div className='bg-hero-3 h-full w-full bg-no-repeat bg-contain bg-top fixed right-0 top-24 -z-10 opacity-50' />
       <header>
         <div className='mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8'>
           <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
